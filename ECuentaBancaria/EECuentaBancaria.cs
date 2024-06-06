22a08836f76e701ef6d6cab842199b9506e243ec
JesusSaucedo22 <jesusemmanuelsaucedocardenas@gmail.com>
2024-06-06 12:02:22 -0600

    Clases interfas nuew

---
+++ ECuentaBancaria/EECuentaBancaria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banco.Modelos
+{
+    public interface ICuentaBancaria
+    {
+        decimal Saldo { get; }
+        void Depositar(decimal cantidad);
+        bool Retirar(decimal cantidad);
+        void RealizarCorteMensual();
+    }
+}
+
